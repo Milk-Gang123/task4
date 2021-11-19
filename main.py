2fab077a64b3f76df462731834f3bb4ad71c9d75
Администратор <alexushveridze@mail.ru>
2021-11-19 23:07:32 +0300

    done

---
+++ main.py
@@ -0,0 +1,29 @@
+import sqlite3
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QInputDialog, QDialog, QTableWidgetItem
+from PyQt5 import uic
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(["id", "Название сорта", "степень прожарки", "молотый/в зернах", "описание вкуса",
+                                                    "цена", "масса упаковки"])
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(152)
+        self.tableWidget.setColumnWidth(0, 20)
+        con = sqlite3.connect("coffee.db")
+        cur = con.cursor()
+        coffes = sorted(list(cur.execute("SELECT * FROM data")), key=lambda x: x[0])
+        for num_x, i in enumerate(coffes):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for num_y, j in enumerate(i):
+                print(num_x, num_y)
+                self.tableWidget.setItem(num_x, num_y, QTableWidgetItem(str(j)))
+
+
+app = QApplication(sys.argv)
+ex = MainWindow()
+ex.show()
+sys.exit(app.exec())
